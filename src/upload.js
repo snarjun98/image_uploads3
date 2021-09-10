9c65626fc1b6b4e58d11b737466addde79c35fc0
snarjun98 <snarjun98@gmail.com>
2021-09-11 00:52:20 +0530

    fix typo error

--- src/upload.js
+++ src/upload.js
@@ -19,7 +19,7 @@ module.exports.handler= async(event)=>{
      const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/, ""), "base64");
      const params = {
      Bucket:BUCKET_NAME,
-     key:`images/${new Date().toISOString()}.jpeg`,
+     Key:`images/${new Date().toISOString()}.jpeg`,
      Body: decodedFile,
      ContentType: "image/jpeg"
      };
