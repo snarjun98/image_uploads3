be8ed66fcb0102dee3ee542e7fc8e6296282af49
snarjun98 <snarjun98@gmail.com>
2021-09-12 00:14:22 +0530

    feat: image resize

--- src/upload.js
+++ src/upload.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk")
 const s3= new AWS.S3();
-
+var Jimp = require('jimp');
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME
 
 module.exports.handler= async(event)=>{
@@ -17,16 +17,40 @@ module.exports.handler= async(event)=>{
      const parsedBody = JSON.parse(event.body);
      const base64File = parsedBody.file;
      const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/, ""), "base64");
-     const params = {
-     Bucket:BUCKET_NAME,
-     Key:`images/${new Date().toISOString()}.jpeg`,
-     Body: decodedFile,
-     ContentType: "image/jpeg"
-     };
-     const uploadResult = await s3.upload(params).promise();
-     response.body= JSON.stringify({
-     message:"Upload success",uploadResult
-     })
+     if (parsedBody.x && parsedBody.y){
+        const resizedFile=Jimp.read(decodedFile)
+                .then(lenna => {
+                    return lenna
+                    .resize(parsedBody.x, parsedBody.y) // resize
+                    .write(`${new Date().toISOString()}-resized.jpeg`); // save
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
+                    const params = {
+                        Bucket:BUCKET_NAME,
+                        Key:`images/${new Date().toISOString()}-resized.jpeg`,
+                        Body: resizedFile,
+                        ContentType: "image/jpeg"
+                        };
+                        const uploadResult = await s3.upload(params).promise();
+                        response.body= JSON.stringify({
+                        message:"Upload success",uploadResult
+                        })
+     }else{
+        const params = {
+            Bucket:BUCKET_NAME,
+            Key:`images/${new Date().toISOString()}.jpeg`,
+            Body: decodedFile,
+            ContentType: "image/jpeg"
+            };
+            const uploadResult = await s3.upload(params).promise();
+            response.body= JSON.stringify({
+            message:"Upload success",uploadResult
+            })
+     }
+     
+    
     }catch(err){
     console.log(err)
     response.body = JSON.stringify({
